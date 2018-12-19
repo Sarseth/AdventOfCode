7a38d1ec288d8241c5ff5d1422d88f541741515e
Sarseth <elune90@gmail.com>
2018-12-19 20:41:22 +0100

    Advent 2015 2nd

--- src/main/java/pl/sarseth/advent/year2015/day02/Year2015Day02.java
+++ src/main/java/pl/sarseth/advent/year2015/day02/Year2015Day02.java
@@ -4,6 +4,7 @@ import pl.sarseth.advent.year2015.pl.sarseth.advent.utils.InputReader;
 
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class Year2015Day02 {
 
@@ -13,43 +14,47 @@ public class Year2015Day02 {
         BigWrapper bigWrapper = new BigWrapper();
         new Scanner(input).forEachRemaining(bigWrapper);
         System.out.println(bigWrapper.wrapSurface);
+        System.out.println(bigWrapper.ribbonLength);
     }
 
     private static class BigWrapper implements Consumer<String> {
 
-        long wrapSurface = 0L;
+        int wrapSurface = 0;
+        int ribbonLength = 0;
 
         @Override
         public void accept(String s) {
             String[] split = s.split("x");
             Rectangle rectangle = new Rectangle(split);
             wrapSurface += rectangle.calculateSurface() + rectangle.calculateSmallestSideSurface();
-            System.out.println(wrapSurface);
+            ribbonLength += rectangle.calculateRibbonLength() + rectangle.calculateVolume();
         }
 
         private static class Rectangle {
 
-            long x,y,z;
+            int x, y, z;
 
             private Rectangle(String[] dimensions) {
-                x = Long.valueOf(dimensions[0]);
-                y = Long.valueOf(dimensions[1]);
-                z = Long.valueOf(dimensions[2]);
+                x = Integer.valueOf(dimensions[0]);
+                y = Integer.valueOf(dimensions[1]);
+                z = Integer.valueOf(dimensions[2]);
             }
 
-            private long calculateSurface() {
-                return 2 * x * y + 2 * x * z + 2 * y * z;
+            private int calculateSurface() {
+                return (2 * x * y) + (2 * x * z) + (2 * y * z);
             }
 
-            private long calculateSmallestSideSurface() {
-                if (x < y) {
-                    if (y < z) {
-                        return x * y;
-                    } else {
-                        return x * z;
-                    }
-                }
-                return y * z;
+            private int calculateSmallestSideSurface() {
+                return IntStream.of(x, y, z).sorted().limit(2).reduce(1, (smallest, mid) -> smallest * mid);
+            }
+
+            private int calculateVolume() {
+                return x * y * z;
+            }
+
+            private int calculateRibbonLength() {
+                int[] smallestSides = IntStream.of(x, y, z).sorted().limit(2).toArray();
+                return smallestSides[0] * 2 + smallestSides[1] * 2;
             }
 
         }
